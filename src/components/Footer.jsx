a95d63bb05836a989dd3b0eb6aa7df28253dff7c
jlienghot <jlienghot@outlook.com>
2026-04-29 18:23:54 -0400

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -41,6 +41,13 @@ function Footer() {
             <FaDev className="text-offWhite hover:text-accent" />
           </a>
         </li>
+        <li className="p-2 sm:mr-5">
+          <a href="https://www.newicon.com/opensauced"
+             target="_blank" rel="noreferrer"
+             aria-label="Visit Open Sauced GitHub Organisation">
+            <NewIcon className="text-offWhite hover:text-accent" />
+          </a>
+        </li>
       </ul>
     </Container>
   );
